451952eb370f117aa024a027ef38451699f81337
anamfazal <anamfazal94@gmail.com>
2020-10-16 21:31:39 +0530

    Added: Program to set single player at 0

---
+++ SnakesLadders.java
@@ -0,0 +1,14 @@
+public class SnakesLadders {
+	
+	public static void main(String[] args) {
+	
+		int[] board=new int[101];
+		int current_player_position=0;
+		for (int i = 0; i <= 100 ; i++) {
+			board[i] = i;
+		}
+
+	}
+
+}
+
