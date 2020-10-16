ff445b99bd65dce5af16fdf7a42910a5586d7e19
anamfazal <anamfazal94@gmail.com>
2020-10-16 22:53:31 +0530

    Refactored: code to store position after every roll and total rolls

--- SnakesLadders.java
+++ SnakesLadders.java
@@ -1,25 +1,26 @@
-/* Purpose: Refactor code to not let player exceed 100 */
+/* Purpose: code to store position after every roll and number of times die was rolled */
+
+import java.util.ArrayList;
 
 public class SnakesLadders {
 		
 	static final int START_POSITION = 0;
 	static final int WINNING_POSITION = 100;
-	static final int NO_PLAY=0;
-	static final int LADDER=1;
-	static final int SNAKE=2;
-	
-	//create instance of game	
+	static final int NO_PLAY = 0;
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
+
+	//create instance of game		
 	private int createGame() {
 		int current_player_position = START_POSITION;
 		return current_player_position;
 	}
 	
-	//get dice number 		
+	//get dice number 
 	private int rollDice() {
 		int roll_result = (int) Math.floor(Math.random() * 10 % 6 + 1 ) ;
 		return roll_result;
 	}
-	
 	//determine no play, ladder or snake move
 	private int getOption(int roll_result , int current_player_position) {
 		int option = (int) Math.floor(Math.random() * 10) % 3 ;
@@ -27,7 +28,7 @@ public class SnakesLadders {
 			case (NO_PLAY):
 				break;
 			case (LADDER):
-				if ((current_player_position + roll_result) <= 100)
+				if ((current_player_position + roll_result) <= 100)			
 					current_player_position += roll_result;
 				break;
 			case (SNAKE):
@@ -35,35 +36,37 @@ public class SnakesLadders {
         				current_player_position = START_POSITION;
 				else
 					current_player_position -= roll_result;
-				break;				
+				break;
+					
 		}		
-
 		return current_player_position;
 	}
-	//play game till player reaches 100
-	private void playGame(int current_player_position,SnakesLadders game1) {
-			while(current_player_position != WINNING_POSITION) {
-    			
-    			if (current_player_position < 0)
-    				current_player_position = START_POSITION;
-    			while(current_player_position != WINNING_POSITION && current_player_position >= 0 ) {
-    				int roll_result=game1.rollDice();
-    				current_player_position = game1.getOption(roll_result , current_player_position );
-    				System.out.print("  "+current_player_position);
-    			}
-    		
+	//print position after every roll and total number of rolls		
+	private void printPosition(ArrayList<Integer> position_list) {
+		for (int count=0 ; count<position_list.size() ; count++ ) {
+    			System.out.println("  " + position_list.get(count));
     		}
+		System.out.println("Total number of dice rolls : " + position_list.size());
 	}
-
-
+		
         public static void main(String[] args) {
         		
-       		SnakesLadders game1=new SnakesLadders();
+        	SnakesLadders game1=new SnakesLadders();		
         	int current_player_position = game1.createGame();
-        	game1.playGame(current_player_position,game1);	
-
-	}
+        		
+        	ArrayList<Integer> position_list = new ArrayList<Integer>();
+       	
+		//loop to create list of poSitions
+	 	while(current_player_position != WINNING_POSITION) {
+       			int roll_result=game1.rollDice();
+        		current_player_position = game1.getOption(roll_result , current_player_position );
+     			position_list.add(current_player_position);
+        	}
+        	game1.printPosition(position_list);
+        		
+        }
 
 }
+
 
 
