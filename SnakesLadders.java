f85125240e1f8ad106233322a03d172646d6abea
anamfazal <anamfazal94@gmail.com>
2020-10-16 21:50:36 +0530

    Refactored:code to make move on basis of 3conditions

--- SnakesLadders.java
+++ SnakesLadders.java
@@ -1,26 +1,49 @@
-/* Purpose: Refactor code to get dice roll result */
+/*Purpose: Check option for no play, ladder and snake */
 
 public class SnakesLadders {
+	//constants	
+	static final int START_POSITION = 0;
+	static final int NO_PLAY=0;
+	static final int LADDER=1;
+	static final int SNAKE=2;
+	
+	//create instance of game	
+	private int createGame() {
 
-	//CONSTANT
-	static final int START_POSITION = 0;		
-
-	//create instance of game
-	private int createGame(int START_POSITION) {
 		int current_player_position = START_POSITION;
 		return current_player_position;
 	}
-	//roll dice and get result
+	//get dice number 	
 	private int rollDice() {
+
 		int roll_result = (int) Math.floor(Math.random() * 10 % 6 + 1 ) ;
 		return roll_result;
 	}
-	
+	//determine no play, ladder or snake move
+	private int getOption(int roll_result , int current_player_position) {
+
+		int option = (int) Math.floor(Math.random() * 10) % 3 ;
+		switch (option) {
+			case (NO_PLAY):
+				break;
+			case (LADDER):
+				current_player_position += roll_result;
+				break;
+			case (SNAKE):
+				current_player_position -= roll_result;
+				break;
+		}		
+		return current_player_position;
+	}
+
         public static void main(String[] args) {
         		
         	SnakesLadders game1=new SnakesLadders();
-       		int current_player_position = game1.createGame(START_POSITION);
+        		
+        	int current_player_position = game1.createGame();
        		int roll_result=game1.rollDice();
+       		current_player_position = game1.getOption(roll_result , current_player_position );
+       		System.out.println(current_player_position);  //display result
         }
 
 }
