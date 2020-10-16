a7d027e61574e054d717122ad01fa553fbb2272b
anamfazal <anamfazal94@gmail.com>
2020-10-16 21:41:48 +0530

    Refactored: code to roll dice and get result

--- SnakesLadders.java
+++ SnakesLadders.java
@@ -1,14 +1,29 @@
-public class SnakesLadders {
-	
-	public static void main(String[] args) {
-	
-		int[] board=new int[101];
-		int current_player_position=0;
-		for (int i = 0; i <= 100 ; i++) {
-			board[i] = i;
-		}
+/* Purpose: Refactor code to get dice roll result */
 
+public class SnakesLadders {
+
+	//CONSTANT
+	static final int START_POSITION = 0;		
+
+	//create instance of game
+	private int createGame(int START_POSITION) {
+		int current_player_position = START_POSITION;
+		return current_player_position;
 	}
+	//roll dice and get result
+	private int rollDice() {
+		int roll_result = (int) Math.floor(Math.random() * 10 % 6 + 1 ) ;
+		return roll_result;
+	}
+	
+        public static void main(String[] args) {
+        		
+        	SnakesLadders game1=new SnakesLadders();
+       		int current_player_position = game1.createGame(START_POSITION);
+       		int roll_result=game1.rollDice();
+        }
 
 }
+
+
 
